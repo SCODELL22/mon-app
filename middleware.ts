e6c3957f0ed8bd3b4c5faff9d2a60a106c88b473
SCODELLARO <scodellaro@MacBook-Air-de-SCODELLARO.local>
2026-06-18 22:33:29 +0200

    ajout protection mot de passe

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (
+      user === process.env.AUTH_USER &&
+      pwd === process.env.AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
